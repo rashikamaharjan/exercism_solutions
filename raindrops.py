89bbe43fbb3b1557a7160ba72564c8db1cf6387c
Rashika Maharjan <rashikamaharjann@gmail.com>
2026-03-17 14:37:28 +0545

    conditionals

---
+++ raindrops.py
@@ -0,0 +1,15 @@
+def convert(number):
+    string = ''
+    if number % 3 == 0:
+        string += 'Pling'
+        
+    if number % 5 == 0:
+        string += 'Plang'
+
+    if number % 7 == 0:
+        string += 'Plong'
+
+    if string == '':
+        return str(number)
+
+    return string
